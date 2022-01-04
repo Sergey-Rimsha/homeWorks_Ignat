b72a19dc70b7d45b1fd2eb6eb54e5f87ced73d41
Sergey <Sergey.rimsha.ru@gmail.com>
2022-01-04 16:54:53 +0300

    lesson_work_0.5

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import style from './Message.module.css'
 
-function Message() {
+type propsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: propsType) {
     return (
-        <div>
+        <div className={style.message} >
+            <img src={props.avatar} alt=""/>
+			<div className={style.wrap}>
+				<div>{props.name}</div>
+				<div>{props.message}</div>
+				<div>{props.time}</div>
+			</div>
 
         </div>
     )
