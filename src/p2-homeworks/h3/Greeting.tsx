38e0a05dd2411acda406221446321c08a12b5207
Sergey <Sergey.rimsha.ru@gmail.com>
2022-01-18 21:50:04 +0300

    lesson_home_work 3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void// need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number   // need to fix any
+    onKeyPressHandler: (e: KeyboardEvent<HTMLInputElement>) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUser, error, totalUsers, onKeyPressHandler} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error ? s.error : s.right; // need to fix with (?:)
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.greeting}>
+            <div className={s.inputWrap}>
+                <input value={name} onKeyPress={onKeyPressHandler} onChange={setNameCallback} className={inputClass}/>
+                <span className={s.errorMessage}>{error}</span>
+            </div>
+            <button disabled={!!error} onClick={addUser}>add</button>
+            <span>{`Total Users: ${totalUsers}`}</span>
         </div>
     )
 }
