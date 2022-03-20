593b78ff8b56eb138be6a10edd079fcc47f0f296
Sergey <Sergey.rimsha.ru@gmail.com>
2022-03-20 13:47:33 +0300

    less: homework 6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import style from './HW6.module.css';
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -10,16 +11,21 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
+        setValue(restoreState('editable-span-value', value));
+
         // setValue()
     }
 
     return (
-        <div>
+        <div className={style.HW6Box}>
             <hr/>
             homeworks 6
 
             {/*should work (должно работать)*/}
             <div>
+                <span className={style.imgWrap}>
+                    <img src={'https://cdn-icons.flaticon.com/png/512/3642/premium/3642497.png?token=exp=1647768720~hmac=8c486feef6ec02e2c587decb3c2db7aa'} alt={'pencil'} />
+                </span>
                 <SuperEditableSpan
                     value={value}
                     onChangeText={setValue}
