98f5b1ca102ccf0ca2c96f170e1f094097a5655e
Sergey.rimsha.ru@gmail.com <Sergey.rimsha.ru@gmail.com>
2022-04-18 09:07:28 +0300

    fix merge

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
 import HW9 from "../../h9/HW9";
-<<<<<<< HEAD
 import HW10 from "../../h10/HW10";
-=======
->>>>>>> main
 
 export const Junior = () => {
 
@@ -14,12 +11,9 @@ export const Junior = () => {
         <>
             <HW7/>
             <HW8/>
-<<<<<<< HEAD
             <HW9/>
             <HW10/>
-=======
             <HW9 />
->>>>>>> main
         </>
     )
 }
